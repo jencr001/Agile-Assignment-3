ef704b527a366dd5649c2049cc2a8decd29150e2
Cassandra Jennings <jencr001@mymail.unisa.edu.au>
2017-06-21 00:06:52 +0930

    Minor UI Chart fix

--- PreferentialVoting/PreferentialVoting/Chart.Designer.cs
+++ PreferentialVoting/PreferentialVoting/Chart.Designer.cs
@@ -46,7 +46,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(689, 351);
             this.Controls.Add(this.PrintButton);
             this.Name = "Chart";
             this.Text = "Initial Vote Chart";
